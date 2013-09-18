ea041cf4aff10b85f07e2138ef9c70170b6a09ef
Paul Chaignon <midionos@laposte.net>
2013-09-18 22:01:19 +0200

    Mode and comments added.

--- TP1/basemenu.pl
+++ TP1/basemenu.pl
@@ -1,3 +1,10 @@
+/**
+ * TP Base Menu
+ * @author Clement GAUTRAIS
+ * @author Paul CHAIGNON
+ * @version Annee scolaire 2013/2014
+ */
+
 hors_d_oeuvre(artichauts_Melanie).
 hors_d_oeuvre(truffes_sous_le_sel).
 hors_d_oeuvre(cresson_oeuf_poche).
@@ -32,27 +39,39 @@ calories(melon_en_surprise, 122).
 
 
 % Question 1.2
+
+% plat_resistance(?)
 plat_resistance(Plat):-
 	viande(Plat);
 	poisson(Plat).
+
+% repas(?, ?, ?)
 repas(Entree, Plat, Dessert):-
 	hors_d_oeuvre(Entree),
 	plat_resistance(Plat),
 	dessert(Dessert).
+
+% bon_plat(?)
 bon_plat(Plat):-
 	calories(Plat, Cal),
 	Cal > 200,
 	Cal < 400.
+
+% plus_calorique_algues(?)
 plus_calorique_algues(Plat):-
 	calories(bar_aux_algues, CalAlgues),
 	calories(Plat, Cal),
 	Cal > CalAlgues.
+
+% total_calories(?, ?, ?, -)
 total_calories(Entree, Plat, Dessert, Total):-
 	repas(Entree, Plat, Dessert),
 	calories(Entree, CalEntree),
 	calories(Plat, CalPlat),
 	calories(Dessert, CalDessert),
 	Total is CalEntree + CalPlat + CalDessert.
+
+% repas_equilibre(?, ?, ?)
 repas_equilibre(Entree, Plat, Dessert):-
 	total_calories(Entree, Plat, Dessert, Total),
 	Total < 800.
--- TP1/basevalois.pl
+++ TP1/basevalois.pl
@@ -1,10 +1,9 @@
 /**
-TP Base Valois - Famille de France
-
-@author Prenom1 NOM1
-@author Prenom2 NOM2
-@version Annee scolaire 20__/20__
-*/
+ * TP Base Valois - Famille de France
+ * @author Clement GAUTRAIS
+ * @author Paul CHAIGNON
+ * @version Annee scolaire 2013/2014
+ */
 
 homme(charles_V).
 homme(charles_VI).
@@ -113,14 +112,21 @@ roi(philippe_VI, de_valois, 1328, 1350).
 
 
 % Question 2.1
+% enfant(?, ?)
 enfant(E, P):-
 	pere(P, E);
 	mere(P, E).
+
+% parent(?, ?)
 parent(P, E):-
 	enfant(E, P).
+
+% grand_pere(+, -)
 grand_pere(G, E):-
 	pere(G, P),
 	parent(P, E).
+
+% frere(+, -)
 frere(F, E):-
 	pere(P, F),
 	pere(P, E),
@@ -128,26 +134,37 @@ frere(F, E):-
 	mere(M, F),
 	homme(F),
 	E \= F.
+
+% soeur(+, -)
 soeur(S, E):-
 	parent(P, S),
 	parent(P, E),
 	femme(S),
 	E \= S.
+
+% frere_ou_soeur(+, -)
 frere_ou_soeur(FS, E):-
 	soeur(FS, E);
 	frere(FS, E).
+
+% oncle(?, ?)
 oncle(O, N):-
 	parent(P, N),
-	frere(O, P).
+	frere(P, O).
+
+% cousin(?, ?)
 cousin(C, E):-
 	parent(P, E),
 	frere_ou_soeur(P, FS),
 	enfant(C, FS).
+
+% le_roi_est_mort_vive_le_roi(+, +, -)
 le_roi_est_mort_vive_le_roi(R1, D, R2):-
 	roi(R1, _, _, D),
 	roi(R2, _, D, _).
 
 % Question 2.2
+% ancetre(+, -)
 ancetre(X,Y):-
 	parent(X,Y).
 ancetre(X,Y):-
